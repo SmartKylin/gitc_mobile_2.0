1356115efb61c6a29419c59fb19c1011feae2690
GayeChen <1580850542@qq.com>
2017-09-18 21:38:55 +0800

    验证码可用性检测，旧的手机号记忆功能

--- src/containers/login_box/index.js
+++ src/containers/login_box/index.js
@@ -2,28 +2,37 @@ import React, {Component} from 'react'
 import './index.scss'
 let iconIphone = require('../../images/icon-phone.svg')
 
-const TIME = 10
+const TIME = 60
 export default class extends Component {
   constructor () {
     super()
     this.state = {
       coding: false,
-      time: TIME
+      time: TIME,
+      mobileRight: false
     }
   }
+  // 发送验证码
+  sendCode() {
+    if (!this.state.mobileRight) {
+      return
+    }
+    this.countDown()
+  }
+  // 倒计时
   countDown() {
     this.setState({
       coding: true
     })
-    let _this = this
+    this.oldMobile = this.mobile.value
     this.timer = setInterval(() => {
       let time = this.state.time;
       if (time <= 0) {
         clearInterval(this.timer)
-        _this.setState({
+        this.setState({
           coding: false,
           time: TIME
-        })
+        });
         return
       }
       time--;
@@ -32,16 +41,32 @@ export default class extends Component {
       })
     }, 1000)
   }
+  // 验证手机号
+  validateMobile(e) {
+    let reg = /^1[3|4|5|7|8][0-9]{9}$/;
+    let mobile = this.mobile.value;
+    let mobileRight =  reg.test(mobile)
+    this.setState({
+      mobileRight
+    });
+    if (mobileRight) {
+      if (this.mobile.value !== this.oldMobile) {
+        this.setState({
+          coding: false
+        })
+      }
+    }
+  }
   render () {
     return (
     <div className="loginbox">
       <div className="mobile--area">
         <div className="mobile--wrap">
           <img src={iconIphone} alt="" className="icon--phone"/>
-          <input type="number"/>
+          <input type="number"  onChange={(e) => this.validateMobile(e)} ref={mobile => this.mobile = mobile}/>
         </div>
         {
-          this.state.coding ? <div className="send--code">重新发送{this.state.time}s</div> : <div className="send--code active" onClick={() => this.countDown()}>发送验证码</div>
+          this.state.coding ? <div className="send--code">重新发送{this.state.time}s</div> : <div className={'send--code ' + (this.state.mobileRight ? 'active' : '')} onClick={() => this.sendCode()}>发送验证码</div>
         }
       </div>
       <div className="code--area">
