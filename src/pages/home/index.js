37553373b9692a86a3a2315213d311c71bf9adc3
731010527@qq.com <731010527@qq.com>
2017-09-21 13:20:43 +0800

    会议议程接口调通

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -21,15 +21,15 @@ const LOGO_2 = require('components/images/logo2.jpg')
 // const HEADER = require('components/images/logo2.jpg')
 // const LOGO_01 = require('components/images/01.png')
 const MAP = require('components/images/map.png')
-
+const blink = require('components/images/b_link.png')
 export default class Activity extends Component {
     constructor(...args) {
         super(...args);
         this.onFetch = this.onFetch.bind(this)
         this.state = {
-            data:null,
-            data1:[],
-            DATAS:[],
+            data: null,
+            data1: [],
+            DATAS: [],
             menuVisible: 'hidden'
         }
         this.onfetchBtn = this.onfetchBtn.bind(this)
@@ -40,12 +40,13 @@ export default class Activity extends Component {
         this.setState({
             data: rooms,
             data1: rooms["DATA23"],
-            DATAS: rooms["DATAS"]
+            DATAS: rooms["DATAS"],
+            toggle: true
         })
 
     }
     componentDidMount() {
-        getPopleList(4).then(data=>data.json()).then(data => {
+        getPopleList(4).then(data => data.json()).then(data => {
             console.log(data)
         })
     }
@@ -53,7 +54,18 @@ export default class Activity extends Component {
         console.log(e.target.innerHTML)
     }
     onFetch(e) {
-        $('.time span').removeClass('catbtn'); 
+        console.log(e.target, 'pppppppp')
+        if (e.target.getAttribute('name') == "DATA23") {
+            this.setState({
+                toggle: true
+            })
+        }
+        else {
+            this.setState({
+                toggle: false
+            })
+        }
+        $('.time span').removeClass('catbtn');
         $(e.target).addClass('catbtn');
         // e.target.removeAttribute('style','color:#fff;')
         this.setState({
@@ -62,31 +74,35 @@ export default class Activity extends Component {
     }
 
     closeMenu = () => {
-      this.setState({
-        menuVisible: 'hidden'
-      })
+        this.setState({
+            menuVisible: 'hidden'
+        })
     }
 
     render() {
         return (
             <span>
-                <div style={{display: 'flex', alignItems: 'center', position: 'absolute', width: '100%'}}>
-                    <a style={{width: '75px', height: '25px', background: '#263c66', lineHeight: '25px', textAlign: 'center', borderRadius: '12px', marginTop: '8px', fontSize: '11px', color: '#fff', position: 'relative', left: '68%'}} href={'http://www.baidu.com'}>立即购买</a>
-                    <div className="menu--icon" onClick={() => this.setState({menuVisible: true})} style={{position: 'absolute', right: '10px', top: '1px'}}/>
-                    <Menu visibility={this.state.menuVisible} closeMenu={this.closeMenu}/>
+                <div class="linkbgtg" style={{ display: 'flex', alignItems: 'center', position: 'absolute', width: '100%' }}>
+                    <div className="ban"></div>
+                    <a className="tolink" style={{ width: '75px', height: '25px', lineHeight: '25px', textAlign: 'center', borderRadius: '12px', marginTop: '8px', fontSize: '11px', color: '#fff', position: 'relative', left: '68%' }} href={'http://www.baidu.com'}>立即购买</a>
+                    <div className="menu--icon" onClick={() => this.setState({ menuVisible: true })} style={{ position: 'absolute', right: '10px', top: '1px' }} />
+                    <Menu visibility={this.state.menuVisible} closeMenu={this.closeMenu} />
                 </div>
-                <div className="index-banner"></div>
+                <div className="index-banner">
+
+                </div>
                 <div className="content">
-                    <div className="highlights">
+                    <div className="highlights highlights">
                         <h3 className="highlights-name ">大会亮点</h3>
                         <p className="highlights-title">ASSEMBLY HIGHLIGHTS</p>
+                        <img src={blink} alt="" className="blue-link" />
                         <ul className="highlights-ul clearfix">
                             <li className="highlights-li ">
                                 {/*<span className="highlights-icon1"></span>*/}
                                 <span className="highlights-icon1"></span>
                                 <div className="highlights-li-font  pr"> 领袖峰会<div className="pa-icon pa-icon1"></div></div>
-                                <div  className="highlights-li-fonts">触电行业最强大脑
-                                    
+                                <div className="highlights-li-fonts">触电行业最强大脑
+
                                 </div>
                             </li>
                             <li className="highlights-li ">
@@ -126,7 +142,7 @@ export default class Activity extends Component {
                                 <div className="highlights-li-font ">黑科技游乐园</div>
                                 <div className="highlights-li-fonts ">感受游戏魅力,最真实表演</div>
                             </li>
-                            <li className="highlights-li " style={{width:'60%'}}>
+                            <li className="highlights-li " style={{ width: '60%' }}>
                                 <span className="highlights-icon9"></span>
                                 <div className="highlights-li-font pr">邀请晚宴
                                     <div className="pa-icon pa-icon3"></div>
@@ -135,9 +151,10 @@ export default class Activity extends Component {
                             </li>
                         </ul>
                     </div>
-                    <div className="special">
+                    <div className="special pas">
                         <h3 className="special-name">大会专题</h3>
                         <p className="special-title">GENERAL ASSEMBLY TOPICS</p>
+                        <img src={blink} alt="" className="blue-link" />
                         <ul className="special-ul ">
                             {this.state.DATAS ?
                                 this.state.DATAS.map((data, index) => {
@@ -152,7 +169,8 @@ export default class Activity extends Component {
                     <div className="pr big">
                         <h3 className="agenda-name">大会议程</h3>
                         <p className="agenda-title">GENERAL ASSEMBLY AGENDA</p>
-                        <Link to="./error.page" className="look-all">查看全部 》</Link>
+                        <img src={blink} alt="" className="blue-link" />
+                        <Link to="./error.page" className="look-all">查看全部 <i className="batn-add"></i></Link>
                     </div>
                     <div className="agenda-content" >
                         <div className="time" onClick={this.onFetch}>
@@ -161,16 +179,56 @@ export default class Activity extends Component {
                         </div>
                         <div className="agenda-ul-box">
                             <div className="agenda-ul-toggle" style={{ display: "block" }}>
-                                <ul className=" agenda-ul agenda-ul-btn1 clearfix">
+                                {/*<ul className=" agenda-ul agenda-ul-btn1 clearfix">
                                     <li className="toggle-bg">主会会场</li>
-                                    {/*<ClassRooms roomList="主会场1" />
+                                    <ClassRooms roomList="主会场1" />
                                     <ClassRooms roomList="主会场2" />
-                                    <ClassRooms roomList="主会场3" />*/}
+                                    <ClassRooms roomList="主会场3" />
                                     {this.state.data1 ?
                                         this.state.data1.map((data, index) => {
                                             return <ClassRooms key={index} roomList={data.name} onfetch={this.onfetchBtn} />
                                         }) : ""}
-                                </ul>
+                                </ul>*/}
+                               {this.state.toggle ?
+                                    <div className="agenda-ul-toggle-box">
+                                        <p className="agenda-ul-toggle-box-one">
+                                            <span>主会场</span>
+                                            <span>运维专场</span>
+                                            <span>大数据专场</span>
+                                            <span>基础架构专场</span>
+                                        </p>
+                                        <p className="agenda-ul-toggle-box-two">
+                                            <span>前端技术专场</span>
+                                            <span>移动互联专场</span>
+                                            <span>IOT峰会</span>
+                                        </p>
+                                        <p className="agenda-ul-toggle-box-three">
+                                            <span>技术管理&产品</span>
+                                            <span>领袖峰会</span>
+                                            <span>企业专场</span>
+                                        </p>
+
+                                    </div>
+
+                                    :  <div className="agenda-ul-toggle-box">
+                                        <p className="agenda-ul-toggle-box-one">
+                                            <span>主会场</span>
+                                            <span>运维专场</span>
+                                            <span>大数据专场</span>
+                                            <span>基础架构专场</span>
+                                        </p>
+                                        <p className="agenda-ul-toggle-box-two">
+                                            <span>质量和测试专场</span>
+                                            <span>网络安全专场</span>
+                                            <span>互联网金融峰会</span>
+                                        </p>
+                                        <p className="agenda-ul-toggle-box-three-r">
+                                            <span>智慧物流论坛</span>
+                                            <span>企业专场</span>
+                                        </p>
+                                    </div>
+
+                                }
                                 <div className="agenda-pople-box agenda-pople-box-btn1" >
                                     <ul className="agenda-pople" >
                                         <AgendaPople />
@@ -212,11 +270,12 @@ export default class Activity extends Component {
                                     </ul>
                                 </div>
                             </div>
-                            <a href="javascript:;" className="btn-all btn-all-bottom " >会议提要 》 </a>
+                            <a href="javascript:;" className="btn-all btn-all-bottom " >议题提交 <i className="batn-r"></i> </a>
                             <div className="guests-popole">
                                 <div className="highlights">
                                     <h3 className="highlights-name ">大会嘉宾</h3>
                                     <p className="highlights-title">ASSEMBLY HIGHLIGHTS</p>
+                                    <img src={blink} alt="" className="blue-link" />
                                 </div>
                                 <span className="title-btn "> 大会主席团	</span>
                                 <ul className="guests-popole-ul clearfix">
@@ -231,7 +290,7 @@ export default class Activity extends Component {
                                     <HeadPortrait name="lp" show="金山云合伙人"  style={{margin:'15px',float:'left'}}/>
                                     <HeadPortrait name="lp" show="携程网携程网携程网" style={{margin:'15px',float:'left'}}/>*/}
                                 </ul>
-                                <a href="#" className="btn-all btn-all-bottoms" >查看更多 》 </a>
+                                <a href="#" className="btn-all btn-all-bottoms" >查看更多 <div className="batn-t"></div>   </a>
                                 <span className="title-btn "> 专家顾问团	</span>
                                 <ul className="guests-popole-ul clearfix">
                                     <li></li>
@@ -241,7 +300,7 @@ export default class Activity extends Component {
                                     <li></li>
                                     <li></li>
                                 </ul>
-                                <a href="#" className="btn-all btn-all-bottoms" >查看更多 》 </a>
+                                <a href="#" className="btn-all btn-all-bottoms" >查看更多 <div className="batn-t"></div>   </a>
                                 <span className="title-btn "> 演讲嘉宾	</span>
                                 <ul className="guests-popole-ul clearfix">
                                     <li></li>
@@ -251,12 +310,13 @@ export default class Activity extends Component {
                                     <li></li>
                                     <li></li>
                                 </ul>
-                                <a href="#" className="btn-all btn-all-bottoms" >查看更多 》 </a>
+                                <a href="#" className="btn-all btn-all-bottoms" >查看更多 <div className="batn-t"></div>   </a>
                             </div>
                             <div className="logo">
                                 <div className="highlights">
                                     <h3 className="highlights-name ">合作伙伴</h3>
                                     <p className="highlights-title">ASSEMBLY HIGHLIGHTS</p>
+                                    <img src={blink} alt="" className="blue-link" />
                                 </div>
                             </div>
                             <div className="logo-contnet">
@@ -264,15 +324,16 @@ export default class Activity extends Component {
                                 <img src={LOGO_1} alt="" />
                                 <p>媒体合作</p>
                                 <img src={LOGO_2} alt="" />
-                                <a href="#" className="btn-all btn-all-bottoms" >赞助机会 》 </a>
+                                <a href="#" className="btn-all btn-all-bottoms" >赞助机会 <div className="batn-r"></div>  </a>
                             </div>
                             <div className="bg-b">
                                 <div className="maps" >
                                     <div className="highlights">
                                         <h3 className="highlights-name ">参会指南</h3>
                                         <p className="highlights-title">ASSEMBLY HIGHLIGHTS</p>
+                                        <img src={blink} alt="" className="blue-link" />
                                     </div>
-                                    <img src={MAP} alt="" />
+                                    <img src={MAP} alt="" className="img" />
                                     <p>时间:2017.11.23-11.24</p>
                                     <p>地址:国家会议中心</p>
                                     <p>乘车地址:地铁十五号线奥林匹克公园站H西南口下车</p>
@@ -281,6 +342,7 @@ export default class Activity extends Component {
                                     <div className="highlights">
                                         <h3 className="highlights-name ">联系我们</h3>
                                         <p className="highlights-title">ASSEMBLY HIGHLIGHTS</p>
+                                        <img src={blink} alt="" className="blue-link" />
                                     </div>
                                     <p className="one">赞助大会&展览展示咨询：business@kylinclub.org</p>
                                     <p>合作单位&合作媒体咨询：gitc@kylinclub.org</p>
