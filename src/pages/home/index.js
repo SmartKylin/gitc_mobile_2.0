116bae40dc4013a37d89b3b48abc9e7461bc4e2f
731010527@qq.com <731010527@qq.com>
2017-09-21 20:40:58 +0800

    解决冲突

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -98,11 +98,6 @@ export default class Activity extends Component {
     render() {
         return (
             <span>
-                {/*<div style={{display: 'flex', alignItems: 'center', position: 'absolute', width: '100%'}}>
-                    <a style={{width: '75px', height: '25px', background: '#263c66', lineHeight: '25px', textAlign: 'center', borderRadius: '12px', marginTop: '8px', fontSize: '11px', color: '#fff', position: 'relative', left: '65%'}} href={'http://www.baidu.com'}>立即购买</a>
-                    <div className="menu--icon" onClick={() => this.setState({menuVisible: true})} style={{position: 'absolute', right: '2%', top: '0'}}/>
-                    <Menu visibility={this.state.menuVisible} closeMenu={this.closeMenu}/>
-                </div>*/}
                 <div className="index-banner">
                     <div className="ban"></div>
                 </div>
