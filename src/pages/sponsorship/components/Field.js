6e055db2e7cf9c133da308c26756dad741bd7410
GayeChen <1580850542@qq.com>
2017-09-25 15:46:23 +0800

    赞助合作页面提交数据成功后，清空表单数据

--- src/pages/sponsorship/components/Field.js
+++ src/pages/sponsorship/components/Field.js
@@ -51,18 +51,24 @@ export default class extends Component {
     })
     this.props.changeValue(this.props.name, value)
   }
+  // 清楚表单数据
+  clearFieldSelf = () => {
+    this.setState({
+      value: ''
+    })
+  }
+  componentDidMount () {
+    this.props.describe(this.clearFieldSelf)
+  }
   render () {
     return (
       <div style={FieldWrap}>
         <div>
           <span>{this.props.title}：</span>
-          {this.props.textArea ? null : <input type={this.props.type || 'text'} style={inputStyle} onInput={ (e) => this.changeValue(e)} onBlur={this.inputValidate}/>}
+          {this.props.textArea ? null : <input value={this.state.value} type={this.props.type || 'text'} style={inputStyle} onInput={ (e) => this.changeValue(e)} onBlur={this.inputValidate}/>}
         </div>
-        {this.props.textArea ? <textarea name="" id="" cols="30" rows="8" maxLength={200} style={{resize: 'none'}} onInput={ (e) => this.changeValue(e)}></textarea> : null}
+        {this.props.textArea ? <textarea name="" id="" cols="30" rows="8" maxLength={200} style={{resize: 'none'}} onInput={ (e) => this.changeValue(e)} value={this.state.value}></textarea> : null}
       </div>
     )
-  }
-  componentWillMount () {
-    console.log(this.props.model);
   }
 }
