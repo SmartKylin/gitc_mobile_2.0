be7709171ee8b617500299ce9cee508ad28e6a3c
GayeChen <1580850542@qq.com>
2017-09-25 14:34:52 +0800

    门票列表加入箭头图标

--- src/pages/ticket/containers/ticketList.js
+++ src/pages/ticket/containers/ticketList.js
@@ -13,12 +13,15 @@ export default class extends Component {
       <div style={{background: '#fff', marginTop: '10%', height: minHeight + 'px', borderRadius: '4px', padding: '0 16px'}}>
         {
           this.props.ticketList && this.props.ticketList.length > 0 ? this.props.ticketList.map((item, index) => (
-              <Link key={index} to={`/ticketdetail/${item.id}`} style={{borderBottom: '1px solid #ccc', height: '54px', fontSize: '14px', lineHeight: '54px', color: '#000', display: 'flex', alignItems: 'center', fontWeight: 'bold'}}>
-                <span className="ticket--avatar">
-                  <img className="avatar--icon" src={avatarIcon} alt=""/>
-                </span>
-                <span style={{width: '70px', }}>{item.name}</span>
-                <span>{item.bt__name}</span>
+              <Link key={index} to={`/ticketdetail/${item.id}`} style={{borderBottom: '1px solid #ccc', height: '54px', fontSize: '14px', lineHeight: '54px', color: '#000', display: 'flex', alignItems: 'center', fontWeight: 'bold', justifyContent: 'space-between'}}>
+                <div>
+                  <span className="ticket--avatar">
+                    <img className="avatar--icon" src={avatarIcon} alt=""/>
+                  </span>
+                  <span style={{width: '70px', display: 'inline-block'}}>{item.name}</span>
+                  <span>{item.bt__name}</span>
+                </div>
+                <div className={'to--detail'}/>
               </Link>
             )
           ) : (
