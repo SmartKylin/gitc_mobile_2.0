a796606f2ecb8750d4b2b2d41771882f6925a203
wj15510151110 <wj15510151110@gmail.com>
2017-09-25 20:46:11 +0800

    大会地图修改图片

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -5,9 +5,9 @@ import ChildBackground from 'containers/child_container'
 import './index.scss'
 import $ from 'jquery'
 
-const img1 = require(`components/images/一.png`)
-const img2 = require('components/images/code.png')
-const img3 = require('components/images/gb.png')
+const img1 = require('components/images/floor1.png')
+const img3 = require('components/images/floor3.png')
+const img4 = require('components/images/floor4.png')
 export default class Map extends Component {
   constructor(...args) {
     super(...args)
@@ -36,13 +36,13 @@ export default class Map extends Component {
           <h4>GITC北京站 一一 {this.state.add}层平面图</h4>
           
           {this.state.add == "一" ? <img src={img1} alt="" className="map-img"/> : ""}
-          {this.state.add == "二" ? <img src={img2} alt="" className="map-img"/> : ""}
           {this.state.add == "三" ? <img src={img3} alt="" className="map-img"/> : ""}
+          {this.state.add == "四" ? <img src={img4} alt="" className="map-img"/> : ""}
         </div>
         
         <div className="map-btn" onClick={this._heandeClick.bind(this)}>
-          <div name="三">4F</div>
-          <div name="二">3F</div>
+          <div name="四">4F</div>
+          <div name="三">3F</div>
           <div name="一" className="btn">1F</div>
         </div>
       </div>
