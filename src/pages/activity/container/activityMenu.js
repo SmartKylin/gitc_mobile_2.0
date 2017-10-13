1e22aec164acc7f6ae1676a6809bd8959c538c0d
lvpengpeng <731010527@qq.com>
2017-10-13 18:23:19 +0800

    现场活动接口替换

--- src/pages/activity/container/activityMenu.js
+++ src/pages/activity/container/activityMenu.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {Menu, Icon} from 'antd'
 import 'antd/dist/antd.css'
 import './index.scss'
-import img from '../../../images/yueguangjifeng.jpg'
+// import img from '../../../images/yueguangjifeng.jpg'
 
 const {SubMenu} = Menu
 
@@ -23,7 +23,7 @@ export default class extends Component {
         {
           this.props.data && this.props.data.map((item, index) => (
             <SubMenu key={`sub${index}`} title={<span style={subMenuStyle} className="activity--title" ><Icon type="file-text" style={{color: '#336cfa', fontSize: '17px'}}/><span>{item.name}</span></span>}>
-              <img src={item.img || img} alt="" style={{width: '100%', padding: '0 10px', height: '200px'}}/>
+              <img src={item.img || ""} alt="" style={{width: '100%', padding: '0 10px', height: '200px'}}/>
               <div style={{padding: '0 15px'}}>
                 <div dangerouslySetInnerHTML={{__html: `${item.content}`}}/>
               </div>
