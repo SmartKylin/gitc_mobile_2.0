19e6a7f153481247939db6276ca3125f2a04374e
wj15510151110 <wj15510151110@gmail.com>
2017-11-11 10:49:53 +0800

    修改bug

--- src/pages/ticket/containers/ticketList.js
+++ src/pages/ticket/containers/ticketList.js
@@ -21,21 +21,23 @@ export default class extends Component {
 
   render () {
     return (
-      <div style={{background: '#fff', marginTop: '10%', minHeight: this.state.minHeight + 'px', borderRadius: '4px', padding: '0 16px'}}>
+      <div style={{background: '#fff', marginTop: '10%', borderRadius: '4px', padding: '0 16px'}}>
         {
           this.props.ticketList && this.props.ticketList.length > 0 ? this.props.ticketList.map((item, index) => (
               item && item.sign_staus != 0?
-              <Link key={index} to={`/ticketdetail/${item.id}/${item.code}`} style={{borderBottom: '1px solid #ccc', height: '54px', fontSize: '14px', lineHeight: '54px', color: '#000', display: 'flex', alignItems: 'center', fontWeight: 'bold', justifyContent: 'space-between'}}>
-                <div>
-                  <span className="ticket--avatar">
-                    <img className="avatar--icon" src={avatarIcon} alt=""/>
-                  </span>
-                  <span style={{width: '70px', display: 'inline-block'}}>{item.name}</span>
-                  <span>{item.bt__name}</span>
-                </div>
-                {/*<div className={'to--detail'}/>*/}
-                <i className="iconfont">&#xe660;</i>
-              </Link>:""
+              <div>
+                <Link key={index} to={`/ticketdetail/${item.id}/${item.code}`} style={{borderBottom: '1px solid #ccc', height: '54px', fontSize: '14px', lineHeight: '54px', color: '#000', display: 'flex', alignItems: 'center', fontWeight: 'bold', justifyContent: 'space-between'}}>
+                  <div>
+                    <span className="ticket--avatar">
+                      <img className="avatar--icon" src={avatarIcon} alt=""/>
+                    </span>
+                    <span style={{width: '70px', display: 'inline-block'}}>{item.name}</span>
+                    <span>{item.bt__name}</span>
+                  </div>
+                  {/*<div className={'to--detail'}/>*/}
+                  <i className="iconfont">&#xe660;</i>
+                </Link>
+              </div>:""
             )
           ) : (
             <div className="no--ticket">
