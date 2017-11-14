deed88e311916f5253283ba3ab6666b0883ded74
GayeChen <1580850542@qq.com>
2017-11-14 18:39:30 +0800

    大会日程大致完成

--- src/pages2/schedule/componets/Headpage/index.js
+++ src/pages2/schedule/componets/Headpage/index.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import dahuiYihceng from '../../images/dahuiyicheng.png'
+// import dahuiYihceng from '../../images/dahuiyicheng.png'
 import './index.scss'
-export default class HeaderPage extends React.Component{
 
-
-
-  render(){
-    return(
-        <div className="HeaderPageBoxBox">
-          <div className="HeaderzPageBox">
-            <div className="HeaderPageBox-bg">
-              主会场(24号)
-            </div>
-            <div className="HeaderPageYXbox">
-              PRESIDIUM
-            </div>
-            <div className="HeaderPageBoxDz">
-              <i className="iconfont icon-positioning" style={{color:"#30508e",marginRight:"3px",fontSize:'18px'}} ></i>1F·大宴会厅C
-            </div>
+export default class HeaderPage extends React.Component {
+  render() {
+    let {name, enName, addr} = this.props
+    
+    return (
+      <div className="HeaderPageBoxBox">
+        <div className="HeaderzPageBox">
+          <div className="HeaderPageBox-bg">
+            {name}
           </div>
-          <div className="HeaderzPageBoxXiang">详情>></div>
+          <div className="HeaderPageYXbox">
+            {enName}
+          </div>
+          <div className="HeaderPageBoxDz">
+            <i className="iconfont icon-positioning" style={{color: "#30508e", marginRight: "3px", fontSize: '18px'}}/>
+            {addr}
+          </div>
         </div>
-
+        <div className="HeaderzPageBoxXiang">详情>></div>
+      </div>
     )
   }
 }
--- src/pages2/schedule/index.js
+++ src/pages2/schedule/index.js
@@ -1,20 +1,82 @@
 import React from 'react'
 import Header from './componets/Headpage'
+import {getAgenda} from "../../services/home";
 import './index.scss'
+import SpeechItem from '../../components2/SpeechItem'
+
 export default class Schedule extends React.Component{
 
   constructor(props){
     super(props)
     this.state = {
-
+      agendaData: []
     }
   }
 
+  async componentWillMount () {
+    let res = await getAgenda()
+    .then(res => res.json())
+    await this.setState({
+      agendaData: res.data
+    })
+    
+  }
 
   render(){
+    let agendaData = this.state.agendaData
+    let obj = {
+      pic:"",
+      stime:"09:00-10:00",
+      stheme:"开幕式直播",
+      company:"",
+      position:"主办方、协办方致辞，大会主席团成员共同亮相启动仪式，正式开启GITC年度盛典。",
+    }
+  
+    let obj1 = {
+      pic:"",
+      stime:"10:00-10:10",
+      stheme:"开场致辞",
+      company:"思科",
+      name:"待定",
+      position:"待定"
+    }
+    let {openPop, closePop, setLoginCb} = this.props
+    console.log(agendaData, 'shefjdls');
     return(
         <div className="ScheduleBox">
-          <Header/>
+          { agendaData.length
+            ? agendaData.map((agenda, index) => (
+              <div>
+                <Header
+                  name={agenda.name}
+                  enName={agenda.json.en || agenda.ename}
+                  addr={agenda.json.addr }
+                />
+  
+                <div className='accor-content'>
+                  {agenda.name.indexOf("23") !=-1 && agenda.main != "主会场" ?<SpeechItem icon="yes" style='yes' speecher={obj}/> :""}
+    
+                  {agenda.name.indexOf("23") !=-1 && agenda.main == "基础架构专场" ? <SpeechItem  speecher={obj1}/> :""}
+                  {
+                    agenda.data && agenda.data.length
+                    ? agenda.data.map((item, ind) => (
+                    <SpeechItem
+                      key={ind}
+                      speecher={item}
+                      
+                      openPop={openPop}
+                      closePop={closePop}
+                      setLoginCb={setLoginCb}
+                    />
+                    ))
+                    : null
+                  }
+                </div>
+              </div>
+            ))
+            : null
+          }
+          
         </div>
     )
   }
