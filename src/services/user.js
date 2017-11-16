6dd84bfcbd82dd332857cf1ad5be862c66fb03c5
GayeChen <1580850542@qq.com>
2017-11-16 18:55:29 +0800

    添加微信接口

--- src/services/user.js
+++ src/services/user.js
@@ -4,3 +4,7 @@ import http from '../fetch'
 export function sign(phone, params) {
   return http.post(api.user + phone + '.json?token=1afb756d16740266efde290917ca1a8e', params)
 }
+
+export function getWeixinConfig(params) {
+  return http.post('http://120.92.10.182:8000/api/weixin/get/token.html?token=1afb756d16740266efde290917ca1a8e', params)
+}
