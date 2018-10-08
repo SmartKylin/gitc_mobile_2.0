0d47e9fe1bb75076ca5d6b2fa096928b9a1d3706
库库素 <kukusu@kukusudeMacBook-Pro.local>
2018-10-08 18:08:20 +0800

    refactor: 1.场馆地图 2.九宫格的英文

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -7,112 +7,118 @@ const getImg = num => require(`../../images/floor_${num}.jpg`)
 
 
 export default class Map extends Component {
-  constructor(...args) {
-    super(...args)
-    this.state = {
-      imgSize: 100,
-      addColor: '#fff',
-      imgPop: 'none',
-      imgAry: [],
-      index: 5
-    }
-  }
-  
-  async componentWillMount() {
-    document.title = "场馆地图"
-    let imgAry = await getListImgs(67).then(res => res.json())
-    // console.log(imgAry.data);
-    await this.setState({
-      imgAry: imgAry.data
-    })
-  }
-  
-  addClick(e) {
-    e.stopPropagation()
-    if (this.state.imgSize >= 240) {
-      return false
-    } else {
-      this.setState({
-        imgSize: this.state.imgSize + 50
-      })
-    }
-  }
-  
-  cutClick(e) {
-    e.stopPropagation()
-    if (this.state.imgSize <= 100) {
-      
-      return false
-    } else {
-      this.setState({
-        imgSize: this.state.imgSize - 50
-      })
-    }
-  }
-  
-  openImg() {
-    this.setState({
-      imgPop: 'block'
-    })
-  }
-  
-  closeImg() {
-    this.setState({
-      imgPop: 'none'
-    })
-  }
-  changeMapImg = ind => {
-    this.setState({
-      index: ind
-    })
-  }
-  render() {
-    let {imgSize, imgPop, imgAry, index }= this.state
-    return (
-      <ChildBackground>
-        {
-          imgAry.length
-          ? (
-            <div>
-              <div className="map-box">
-                <div className="map-img-box">
-                  <h4 className="map-header">{imgAry[index].content}</h4>
-                  <img src={imgAry[index].img} alt="" className="map-img" onClick={this.openImg.bind(this)}/>
-                </div>
-    
-                <div className="map-btn" >
-                  {
-                    
-                    imgAry.map((item, ind) => (
-                      <div key={ind} className={index == ind ? 'btn' : ''} onClick={() => this.changeMapImg(ind)}>{imgAry[ind].title}</div>
-                    ))
-                  }
-                </div>
-              </div>
-              <div className="img-pop" style={{display: imgPop}} onClick={this.closeImg.bind(this)}>
-                <div className="img-container">
-                  <img
-                    // src={getImg(add)}
-                    src={imgAry[index].img}
-                    className="map-img img-size"
-                    style={{width: `${imgSize}%`, height: `${imgSize}%`}}
-                  />
-                </div>
-                <div className="map-scal">
-                  <div className="btn-link" onClick={this.addClick.bind(this)}
-                       style={{background: imgSize >= '250' ? '#ccc' : "#fff"}}>+
-                  </div>
-                  <div className="divbottom" onClick={this.cutClick.bind(this)}
-                       style={{background: imgSize <= '100' ? '#ccc' : "#fff"}}>-
-                  </div>
-                </div>
-              </div>
-            </div>
-          )
-          : null
+    constructor(...args) {
+        super(...args)
+        this.state = {
+            imgSize: 100,
+            addColor: '#fff',
+            imgPop: 'none',
+            imgAry: [],
+            index: 5
         }
-      </ChildBackground>
-    )
-  }
+    }
+
+    async componentWillMount() {
+        document.title = "场馆地图"
+        let imgAry = await getListImgs(67).then(res => res.json())
+        // console.log(imgAry.data);
+        await this.setState({
+            imgAry: imgAry.data
+        })
+    }
+
+    addClick(e) {
+        e.stopPropagation()
+        if (this.state.imgSize >= 240) {
+            return false
+        } else {
+            this.setState({
+                imgSize: this.state.imgSize + 50
+            })
+        }
+    }
+
+    cutClick(e) {
+        e.stopPropagation()
+        if (this.state.imgSize <= 100) {
+
+            return false
+        } else {
+            this.setState({
+                imgSize: this.state.imgSize - 50
+            })
+        }
+    }
+
+    openImg() {
+        this.setState({
+            imgPop: 'block'
+        })
+    }
+
+    closeImg() {
+        this.setState({
+            imgPop: 'none'
+        })
+    }
+
+    changeMapImg = ind => {
+        this.setState({
+            index: ind
+        })
+    }
+
+    render() {
+        let {imgSize, imgPop, imgAry, index} = this.state
+        return (
+            <ChildBackground>
+                {
+                    imgAry.length
+                        ? (
+                            <div>
+                                <div className="map-box">
+                                    <div className="map-img-box">
+                                        <h4 className="map-header">{imgAry[index].content}</h4>
+                                    </div>
+                                    <div className='img-text'>
+                                        <img src={imgAry[index].img} alt="" className="map-img"
+                                             onClick={this.openImg.bind(this)}/>
+                                        <div className="map-btn">
+                                            {
+
+                                                imgAry.map((item, ind) => (
+                                                    <div key={ind} className={index == ind ? 'btn' : ''}
+                                                         onClick={() => this.changeMapImg(ind)}>{imgAry[ind].title}</div>
+                                                ))
+                                            }
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div className="img-pop" style={{display: imgPop}} onClick={this.closeImg.bind(this)}>
+                                    <div className="img-container">
+                                        <img
+                                            // src={getImg(add)}
+                                            src={imgAry[index].img}
+                                            className="map-img img-size"
+                                            style={{width: `${imgSize}%`, height: `${imgSize}%`}}
+                                        />
+                                    </div>
+                                    <div className="map-scal">
+                                        <div className="btn-link" onClick={this.addClick.bind(this)}
+                                             style={{background: imgSize >= '250' ? '#ccc' : "#fff"}}>+
+                                        </div>
+                                        <div className="divbottom" onClick={this.cutClick.bind(this)}
+                                             style={{background: imgSize <= '100' ? '#ccc' : "#fff"}}>-
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                        : null
+                }
+            </ChildBackground>
+        )
+    }
 }
 
