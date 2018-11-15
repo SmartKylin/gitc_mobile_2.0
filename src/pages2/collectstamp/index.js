c4806fd2bd152dd4f32c41e1c74024aaca0aaabd
wj15510151110 <wj15510151110@gmail.com>
2018-11-15 18:42:30 +0800

    印章图片更换

--- src/pages2/collectstamp/index.js
+++ src/pages2/collectstamp/index.js
@@ -5,7 +5,7 @@ export default class extends Component {
   render () {
     return (
       <div className="router-page" style={{paddingTop: '20px'}}>
-        <img src={CollectStampImg} alt="" style={{width: '100%', height: '7.53rem'}}/>
+        <img src={CollectStampImg} alt="" style={{width: '100%'}}/>
       </div>
     )
   }
